cf99731b2631699abc0e3a837b8b3352739f78ad
Ntekim-Jotham <joannes4real@gmail.com>
2019-09-23 19:26:26 +0100

    tables migrated and env files added

---
+++ database/migrations/2019_09_23_181550_create_todos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('author');
+            $table->string('title');
+            $table->string('description');
+            $table->string('completedTodos');
+            $table->string('totalTodos');
+            $table->boolean('status');
+            $table->boolean('isArchived');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
