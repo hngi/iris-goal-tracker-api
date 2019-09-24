db5611a04ef9dec411d9f349b5b6e7fcbd88409e
Smahrt <msmahrt@gmail.com>
2019-09-24 21:33:38 +0100

    bug fixes

--- lib/server-response.ts
+++ lib/server-response.ts
@@ -1,11 +1,8 @@
-import { PaginateResponse } from "../handlers/pagination.handler";
-
 export class ServerResponse {
-  constructor(public status: string, public code: string, public message: string, public data = null, public meta: PaginateResponse | null = null) {
+  constructor(public status: string, public code: string, public message: string, public data = null) {
     this.status = status;
     this.code = code;
     this.message = message;
     this.data = data;
-    this.meta = meta;
   }
 }
