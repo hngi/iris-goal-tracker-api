3d66a3ca865d265df22c0bcc4937b51bcaec9a53
Smahrt <msmahrt@gmail.com>
2019-10-05 20:57:57 +0100

    bug fixes

--- services/index.ts
+++ services/index.ts
@@ -1,10 +1,12 @@
 import userEndpoints from "./users/users.endpoint";
 import goalEndpoints from "./goals/goals.endpoint";
 import todoEndpoints from "./todos/todos.endpoint";
+import feedbackEndpoints from "./feedbacks/feedbacks.endpoint";
 
 
 export default [
   ...userEndpoints,
   ...goalEndpoints,
-  ...todoEndpoints
+  ...todoEndpoints,
+  ...feedbackEndpoints
 ]
